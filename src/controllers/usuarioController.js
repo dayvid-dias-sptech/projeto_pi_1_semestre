07bbf58694263b63ae844d6df7356cb5d8b011ad
dayvid-dias-sptech <dayvid.dias@sptech.school>
2026-05-18 00:13:21 -0300

    feat: adiciona cadastro e login funcional, e adiciona página de cadastro de herói

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -3,6 +3,7 @@ var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
+    var nickname = req.body.nicknameServer;
     var senha = req.body.senhaServer;
 
     if (email == undefined) {
@@ -20,13 +21,14 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
+                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].idUsuario)
                             .then((resultadoAquarios) => {
                                 if (resultadoAquarios.length > 0) {
                                     res.json({
                                         id: resultadoAutenticar[0].id,
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
+                                        nickname: resultadoAutenticar[0].nickname,
                                         senha: resultadoAutenticar[0].senha,
                                         aquarios: resultadoAquarios
                                     });
@@ -56,7 +58,8 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
+    var nickname = req.body.nicknameServer;
+    var foto = req.body.fotoServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -65,12 +68,14 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
+    } else if (nickname == undefined) {
+        res.status(400).send("Seu nickname está undefined!");
+    } else if (foto == undefined){
+        res.status(400).send("Sua foto está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nome, nickname, email, senha, foto)
             .then(
                 function (resultado) {
                     res.json(resultado);
